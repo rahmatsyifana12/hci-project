cd40acb3df2865f89282abd6ba9a0f3a0cf44956
rahmatsyifana12 <adean435@gmail.com>
2021-07-06 12:12:43 +0700

    updating portfolio

--- portfolio-script.js
+++ portfolio-script.js
@@ -11,4 +11,12 @@ $(document).ready( () => {
         $('.navbar .menu').toggleClass('appear');
         $('.menu-bar i').toggleClass('appear');
     }); 
+
+    // var scroll_link = $('.point');
+    // scroll_link.click(function(e){
+    //     e.preventDefault();
+    //     $('body,html').animate({
+    //         scrollTop: $(this.hash).offset().top
+    //     }, 1000);
+    // });
 });
